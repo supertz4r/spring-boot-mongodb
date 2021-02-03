8d9bc81588fd900782f7ce1b999b66b283bb91f3
Júnior Oliveira <juninsoader@gmail.com>
2021-02-03 11:20:52 -0300

    obtendo um usuário por id

---
+++ src/main/java/com/jr/springbootmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jr.springbootmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
